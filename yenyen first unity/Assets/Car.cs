587a3c63deb3bb12e7d8ec8d39f1a29e748e4bf4
phiee <63327436+phiee@users.noreply.github.com>
2020-04-08 11:54:08 +0800

    新增C#
    第一個C#

---
+++ yenyen first unity/Assets/Car.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+// 雙斜線 註解，可用在說明或是紀錄上，只會在腳本內看到
+// summary 摘要。描述此內容，會在其他腳本內看到
+
+/// <summary>
+///定義汽車物件
+/// </summary>
+
+public class Car : MonoBehaviour
+{
+    // 認識第一個成員： 欄位 field -儲存物件資料
+    // 資料類型
+    // c.c.數   - 2000      整數    int    
+    // 重量     - 100.9     浮點數  float  - C#中只要用到浮點數，後面一定要加f(或是F)
+    // 品牌     - BMW       文字    string 
+    // 剎車     - 是否開啟  布林值  bool   - true/false
+
+    // 定義欄位語法：
+    // 資料類型 欄位名稱 = 指定值;
+    // 欄位名稱如果用到保留字顏色會變
+    // 請注意欄位名稱不要使用數字開頭或是保留字
+    // 預設值： int‧float 為 0
+    // string 為 ""
+    // bool 為 false
+   
+    int cc = 2000;
+    float weight = 100.9f;
+    string brand = "BMW"; 
+    bool brake = false;
+}
---
+++ yenyen first unity/Assets/Class1.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;    // 引用 Unity提供的API(函式庫:Unity功能與屬性) . 2020年4月8日學習
+
+// 類別 class 定義一個物件
+// 語法：class 類別名稱
+// ：Monobehaviour 可以將此腳本掛在Unity 物件上
+public class Class1 : MonoBehaviour
+{
+    // 定義類別成員
+    // C#
+    // 1.大小不一樣
+    // 2.括號成對出現() [] {} '' "" < >
+
+    // 事件：在特定時間會以指定次數執行的方法 ex:遊戲開始的時候執行OP、TITLE動畫LOOP等 (名稱為藍色)
+    // 開始事件：播放遊戲時執行一次 - ex:指定初始設定，如剛開始的所持金等
+    private void Start()
+    {
+        // 輸出("文字訊息");
+        // C# 結尾為分號;
+        print("hello world");
+    }
+}
