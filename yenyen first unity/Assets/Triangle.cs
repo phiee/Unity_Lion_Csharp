e2a490ec53e9ee0bbcc6a333a583ec58296b6d85
phiee <63327436+phiee@users.noreply.github.com>
2020-06-17 16:19:51 +0800

    146

---
+++ yenyen first unity/Assets/Triangle.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// 三角形 繼承 形狀
+/// 三角形體積需要複寫
+/// </summary>
+
+public class Triangle : Shape
+{
+
+    /// <summary>
+    /// override 複寫：複寫類別
+    /// </summary>
+    /// <returns></returns>
+    public override float GetVolume()
+    {
+        volume = length * width * height / 2; //三角形體積
+        return volume;
+    }
+
+    private void Start()
+    {
+        print(gameObject + "體積：" + GetVolume());
+    } 
+}
